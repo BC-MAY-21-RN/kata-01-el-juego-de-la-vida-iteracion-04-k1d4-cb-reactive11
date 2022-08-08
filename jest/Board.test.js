ea5adbc702a81ef26c135cb6a52ee015de604dab
Ch4s3d <ramozchris@gmail.com>
2022-08-08 13:54:12 -0500

    2 new Tests

--- jest/Board.test.js
+++ jest/Board.test.js
@@ -12,3 +12,19 @@ test('Advances Cicles', () => {
   expect(board.Cicles()).not.toBe(CellsBefore);
 });
 
+test('Applies rules for single cell', () => {
+  board.cells[1][1] = 0;
+  expect(board.Rules(1, 1, 3)).toBe(1);
+});
+
+test('Counts Neighbours', () => {
+  var x = 2
+  var y = 2
+  for (var i = -1; i <= 1; i++) {
+    for (var j = -1; j <= 1; j++) {
+      board.cells[x + j][y + i] = 1;
+    }
+  }
+  espect(board.neighbourCounter(x, y)).toBe(8);
+});
+
