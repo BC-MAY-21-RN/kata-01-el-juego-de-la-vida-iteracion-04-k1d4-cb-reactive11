3bfb70fed16da5e7cd9012cdd137f4cb9fbeafe5
cleon5 <cleon5@ucol.mx>
2022-08-08 09:28:06 -0500

    Correcciones

--- Board.js
+++ Board.js
@@ -2,6 +2,8 @@ module.exports = class Board {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+    this.array = [];
+    this.score = [];
     this.table = this.createDimensions();
   }
   createDimensions() {
@@ -30,8 +32,7 @@ module.exports = class Board {
           for (let k = j - 1; k <= j + 1; k++) {
             if (l <= -1 || l >= array.length || k <= -2 || k >= array[1].length)
               break;
-            if (array[l][k] == 1) 
-              cont++;
+            if (array[l][k] == 1) cont++;
           }
         }
         pun.push(cont);
@@ -44,26 +45,30 @@ module.exports = class Board {
   createGeneration(score, array) {
     let result = [];
     let res = [];
+    this.array = array;
+    this.score = score;
     for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < array[1].length; j++) {
-        if (array[i][j] == 1) {
-          if (score[i][j] == 3 || score[i][j] == 4)
-            //Cualquier celda viva con dos o tres vecinos vivos vive en la próxima generación.
-            res.push(1);
-          else if (score[i][j] < 3 || score[i][j] > 4)
-            //celula muere si tiene menos de 2 celulas vivas vecinas
-            //celula muere si tiene mas de 3 celulas vivas vecinas
-            res.push(0);
-        }
-        //Cualquier celda muerta con exactamente tres vecinos vivos se convierte en una celda viva.
-        else if (array[i][j] == 0 && score[i][j] == 3) {
-          res.push(1);
-        } else res.push(0);
+        res.push(this.rules(i, j));
       }
-
       result.push(res);
       res = [];
     }
     return result;
+  }
+  rules(i, j) {
+    if (this.array[i][j] == 1) {
+      if (this.score[i][j] == 3 || this.score[i][j] == 4)
+        //Cualquier celda viva con dos o tres vecinos vivos vive en la próxima generación.
+        return 1;
+      else if (this.score[i][j] < 3 || this.score[i][j] > 4)
+        //celula muere si tiene menos de 2 celulas vivas vecinas
+        //celula muere si tiene mas de 3 celulas vivas vecinas
+        return 0;
+    }
+    //Cualquier celda muerta con exactamente tres vecinos vivos se convierte en una celda viva.
+    else if (this.array[i][j] == 0 && this.score[i][j] == 3) {
+      return 1;
+    } else return 0;
   }
 };
--- JV.js
+++ JV.js
@@ -2,17 +2,20 @@ const Board = require("./Board");
 //Variables para crear la tabla
 let x = 6;
 let y = 4;
-Repetir(3);
+let repeticiones = 3
 
-function Repetir(int) {
+Generaciones(repeticiones);
+
+function Generaciones(int) {
   let firstGen = new Board(x, y);
   let punt;
   let arr = firstGen.table;
-  for (let i = 1; i <= int; i++) {
-    if (i == 1) console.table(arr);
+  for (let i = 1; i <=int; i++) {
+    console.log('Generacion #'+i);
+    console.table(arr);
     punt = firstGen.createScore(arr);
     arr = firstGen.createGeneration(punt, arr);
     //console.table(punt)
-    console.table(arr);
+    //console.table(arr);
   }
 }
